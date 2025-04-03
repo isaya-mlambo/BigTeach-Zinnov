37c1f71ab29f6b82f708e5df7575eab96e51a67d
Isaya Mlambo <isayamlambo@gmail.com>
2025-04-03 15:42:36 +0200

    Project Initialization

---
+++ zinnov-app/src/main/java/com/example/zinnov_app/ZinnovAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.zinnov_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZinnovAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZinnovAppApplication.class, args);
+	}
+
+}
